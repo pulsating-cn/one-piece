78c738e84dde1beab84d9ec56a4dc9855b8b803e
liuxy <ccliuxy@gmail.com>
2019-06-23 02:01:26 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,23 @@
 <template>
-  <div class="home">
-		<p>{{ url }}</p>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png">
+		<HelloWorld msg="Welcome to Your Vue.js App" />
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+	// @ is an alias to /src
+	import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  },
-	data(){
+	export default {
+		name: 'home',
+		components: {
+			HelloWorld
+		},
+		data() {
 			return {
 				url: process.env.VUE_APP_URL,
-				text: 'nihao'
 			}
+		}
 	}
-}
 </script>
