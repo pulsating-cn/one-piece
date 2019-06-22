3e09181b5f6f2cb33f66e337c9a3e39016b20cb9
liuxy <ccliuxy@gmail.com>
2019-06-23 01:45:25 +0800

    eslint是否开启

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,6 @@ module.exports = {
 	publicPath: '/' ,//跟路径
 	outputDir: "dist",//构建输入目录
 	assetsDir: "assets",//静态资源目录(js, css, img, fonts)
+	lintOnSave: false,//是否开启eslint检测,有效值: true || false || 'error'
+	
 };
