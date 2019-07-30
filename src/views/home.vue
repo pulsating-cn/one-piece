381c47137c9bcaa0a689dccf4b66249ecb225bd0
liuxy <2199842341@qq.com>
2019-07-30 15:13:13 +0800

    Create home.vue

---
+++ src/views/home.vue
@@ -0,0 +1,34 @@
+<template> 
+	<div class="home">
+		<img class="home-bg" alt="bg-xiuxiu" src="/image/main-bg/bg-xiuxiu.jpg">
+	</div>
+</template>
+
+
+<script>
+
+	export default {
+		name: 'home',
+		components: {
+			
+		},
+		data(){
+			return {
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+
+<style>
+	.home-bg {
+		position: absolute;
+		right: 2rem;
+		bottom: 3rem;
+		width: 16rem;
+		
+	}
+</style>
